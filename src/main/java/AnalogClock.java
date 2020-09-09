9245b8280700b213b0c0fea2f5823cf6e6c9ae75
DavidOchoa20 <70774820+DavidOchoa20@users.noreply.github.com>
2020-09-09 09:57:28 -0700

    Update AnalogClock.java

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,19 +7,13 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        int degInHour;
-        int currHour;
-        int currMins;
-        int angOfMin;
         int degHour = scanner.nextInt();
 
         //Determines hours and the minutes
-        degInHour = 30;
-        currHour = degHour / degInHour;
-        currMins = degHour % degInHour * 2;
+        int currMins = degHour % 30 * 2;
 
         //Detemines the angle of min hand
-        angOfMin = currMins * 360 / 60;
+        int angOfMin = currMins * 360 / 60;
         System.out.println(angOfMin);
 
         // closing the scanner object
