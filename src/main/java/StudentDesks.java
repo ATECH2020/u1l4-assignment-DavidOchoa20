610dee96b155beddcb17903da928c053d75fcbdf
DavidOchoa20 <70774820+DavidOchoa20@users.noreply.github.com>
2020-09-09 09:09:34 -0700

    Update StudentDesks.java

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,23 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
+        System.out.println("Students in group 1: ");
+        int group1Count = scanner.nextInt();
+        System.out.println("Students in group 2: ");
+        int group2Count = scanner.nextInt();
+        System.out.println("Students in group 3: ");
+        int group3Count = scanner.nextInt();
+
+        //Calcs desks Needed
+        int deskSpace = 2;
+        int deskNeeded = (group1Count / deskSpace + group2Count / deskSpace + group3Count / deskSpace);
+        if (group1Count % deskSpace != 0) deskNeeded++;
+        if (group2Count % deskSpace != 0) deskNeeded++;
+        if (group3Count % deskSpace != 0) deskNeeded++;
+
+        //Outputs
+        System.out.println("Desks Needed: " + deskNeeded);
         
-
-        /*
-         *  your code goes here
-         */
-
 
         // closing the scanner object
         scanner.close();
